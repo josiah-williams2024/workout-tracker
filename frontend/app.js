b88b94998f3a3090081500971733d16979f28536
josiah-williams2024 <josiahjaw78@live.ca>
2025-11-06 18:50:16 -0500

    Created the Default Workout form with js, css and html

---
+++ frontend/app.js
@@ -0,0 +1,55 @@
+//store all workouts in an array
+const workouts = [];
+
+//Select DOM elements
+const form = document.getElementById('workoutForm');
+const workoutList = document.getElementById('workoutList');
+
+//Handle Form Submission
+form.addEventListener("submit",(e) => {
+e.preventDefault(); //Stop page reload
+
+//Read input values
+const name = document.getElementById('workoutName').value.Trim();
+const type = document.getElementById('workoutType').value;
+const duration = document.getElementById('workoutDuration').value;
+const date = document.getElementById('date').value;
+
+//Create a wrokout object
+const workout = {
+    name,
+    type,
+    duration, 
+    date
+};
+
+//Add workout to array
+workouts.push(workouts);
+
+//Clear form
+form.reset();
+
+//Refresh displayed list
+displayWorkouts();
+});
+
+/* ===========================================================
+Function: displayWorkouts
+Description: Displays the list of workouts
+Parameters: None
+Returns: None
+==============================================================*/
+function displayWorkouts(){
+workoutList.innerHTML = "";
+
+workouts.forEach((workout) => {
+const list = document.createElement('list');
+list.innerHTML = `
+<strong>${workout.name}</strong> (${workout.type})<br>
+Duration: ${workout.duration} mins <br>
+Date: ${workout.date}
+`;
+workoutList.appendChild(list);
+});
+}
+
